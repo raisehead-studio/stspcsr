36204011c3e3643733813f112d5f2a6fc9a20aa6
Hinrick <hinrick71@gmail.com>
2023-07-13 21:25:27 +0800

    add github action config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
-
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
 const repo = "stspcsr_host";
-const assetPrefix = `/${repo}/`;
-const basePath = `/${repo}`;
+let assetPrefix = "";
+let basePath = "/";
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
 
 const nextConfig = {
   images: {
